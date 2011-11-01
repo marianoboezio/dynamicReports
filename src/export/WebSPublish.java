621cae300e17282866ab9ba59293d4240155a541
Mariano Boezio <mariano@zimmic.com>
2011-11-01 21:48:19 -0200

    trying to publish ws

--- src/export/WebSPublish.java
+++ src/export/WebSPublish.java
@@ -1,18 +1,34 @@
 package export;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.ws.Endpoint;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
+import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRXmlDataSource;
+import net.sf.jasperreports.engine.export.JRXlsExporter;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
 import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
 
 
 public class WebSPublish extends HttpServlet {
@@ -75,8 +91,77 @@ public class WebSPublish extends HttpServlet {
 		}		
     }
 	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String xml = "<root><User>Travis Brooks 85.19 % Coverage Conflict</User><conflicts><conflict><acount> Ag Workers Ins Grp </acount><ccoverage> Y </ccoverage>" +
+					"<eemployee> O Hennesey, Edward </eemployee><productt> US-HG </productt><productionytd> $197025.91 </productionytd><prospectiveEmpR></prospectiveEmpR><exitingEmpR></exitingEmpR>"+			
+					"</conflict></conflicts></root>";
+		XmlExportWS tmp = new XmlExportWS();		
+		
+		try { 
+			
+			Document xmlOutput = xmlFormat(xml);
+			
+			// Create Data source
+			JRXmlDataSource xmlDataSource = new JRXmlDataSource(xmlOutput, "root/conflicts/conflict");	 
+			
+			// Complie Template to .jasper
+			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+			System.out.println(classLoader.getResourceAsStream("coverage.jrxml"));
+			JasperReport jasperReport = JasperCompileManager.compileReport(classLoader.getResourceAsStream("coverage.jrxml"));
+
+			/* JasperPrint is the object contains
+			report after result filling process */
+			JasperPrint jasperPrint;	 	
+			
+			// filling report with data from data source
+			jasperPrint = JasperFillManager.fillReport(jasperReport,null,xmlDataSource);
+			
+			File temp = File.createTempFile("report", "");
+			DataHandler dataHandler = new DataHandler(new FileDataSource(temp));
+			
+			// exports to xls file
+			JRXlsExporter exporterXls = new JRXlsExporter ();
+			exporterXls.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+			exporterXls.setParameter(JRExporterParameter.OUTPUT_STREAM,dataHandler.getOutputStream());
+			exporterXls.exportReport();	
+		
+			//FileInputStream fileInputStream = null;
+			 
+			byte[] bFile = new byte[((FileInputStream)dataHandler.getContent()).available()];
+	 
+			//convert file into array of bytes
+			((FileInputStream) dataHandler.getContent()).read(bFile);
+			((FileInputStream) dataHandler.getContent()).close();
+ 
+			for (int i = 0; i < bFile.length; i++) {
+				resp.getWriter().print((char)bFile[i]);
+			}
+ 
+			//System.out.println("Done");
+
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}	
+		
+	}
+
+	public Document xmlFormat(String input) {
+	    try { 
+	    	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	    	DocumentBuilder builder = factory.newDocumentBuilder();
+	    	Document document = builder.parse(new InputSource(new StringReader(input)));
+	    		        
+	        return document;
+	    } catch (Exception e) {
+	        throw new RuntimeException(e); 
+	    }
+	}
 	
-	   
+	
+	 /*  
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
@@ -134,13 +219,13 @@ public class WebSPublish extends HttpServlet {
 			
 			//resp.getWriter().println(new String(out, "UTF-8"));			
 			
-			*/
+			*
 		} catch (Exception e) {
 			
 			e.printStackTrace();
 		}	
 		
-	}
+	}*/
 
 
 
