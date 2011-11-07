5ee41fb465ecbf0472f65e43b050989967c2d291
Mariano Boezio <mariano@zimmic.com>
2011-11-07 19:39:39 -0200

    trying 7:33 11/07/2011

--- src/export/WebSPublish.java
+++ src/export/WebSPublish.java
@@ -46,10 +46,11 @@ public class WebSPublish extends HttpServlet {
 		
 		System.out.println();System.out.println();
 		
-		for (Enumeration e = req.getParameterNames() ; e.hasMoreElements();)
-		       System.out.println(e.nextElement().hashCode());	
+		for (Enumeration e = req.getParameterNames() ; e.hasMoreElements();) {
+		       System.out.println(e.nextElement());	
+		}
 		
-		System.out.println(req.getParameterNames().toString());
+		//System.out.println(req.getParameterNames().toString());
 		System.out.println(req.toString());
 		System.out.println();System.out.println();
 		
