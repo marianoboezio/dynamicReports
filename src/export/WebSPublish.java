aa52a1252c8c24ce52c3cc580e7a4ea02986cd2d
Mariano Boezio <mariano@zimmic.com>
2011-11-30 16:18:26 -0200

    64decode decrypt 9

--- src/export/WebSPublish.java
+++ src/export/WebSPublish.java
@@ -58,7 +58,7 @@ public class WebSPublish extends HttpServlet {
 	String key = keypass[1];*/
 	
 	Integer length = Integer.valueOf(EncryptedPASSWORDKEY.substring(EncryptedPASSWORDKEY.length() - 2));
-	String key = EncryptedPASSWORDKEY.substring(length - 1 , EncryptedPASSWORDKEY.length() - 3);
+	String key = EncryptedPASSWORDKEY.substring(length , EncryptedPASSWORDKEY.length() - 3);
 	String EncryptedPASSWORD = EncryptedPASSWORDKEY.substring(0 , length - 1);
 	
 	System.out.println("######################## Encrypted CREDENTIALS ########################");
