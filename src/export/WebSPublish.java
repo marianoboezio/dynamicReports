ae33fbbb0e3d2e3e133ebc027e5d0db4fa5b6540
Mariano Boezio <mariano@zimmic.com>
2011-12-12 15:56:43 -0200

    timeout error 23

--- src/export/WebSPublish.java
+++ src/export/WebSPublish.java
@@ -245,7 +245,7 @@ public class WebSPublish extends HttpServlet {
 		    
 		    
 		    connection = Connector.newConnection(config);   
-		    QueryResult queryResults =  connection.query("SELECT o.Row_HTML__c, o.Object_Export_Excel__c FROM Object_Row__c o WHERE o.Object_Export_Excel__c = 'a1eG0000000DWiv' ORDER BY o.name");// + reportID + "' ORDER BY o.name");	
+		    QueryResult queryResults =  connection.query("SELECT o.Row_HTML__c, o.Object_Export_Excel__c FROM Object_Row__c o WHERE o.Object_Export_Excel__c = '" + reportID + "' ORDER BY o.name");	
 		    		    
 		    System.out.println("######################## START ########################");
 		    
